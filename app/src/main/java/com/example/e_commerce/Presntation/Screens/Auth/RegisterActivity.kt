a7d2523231061f4192b0a527a24b64a0e64ae346
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-10-06 16:29:59 +0300

    refactor: removing unused import directive

--- app/src/main/java/com/example/e_commerce/Presntation/Screens/Auth/RegisterActivity.kt
+++ app/src/main/java/com/example/e_commerce/Presntation/Screens/Auth/RegisterActivity.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
